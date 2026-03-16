9d88b1fcf5004ac3e892395de99796f5e14bd7f1
ralph <sanghwaj222@gmail.com>
2026-03-16 11:21:43 +0900

    TestController 완료, 검증 준비

--- Jinary-Backend/src/main/java/jinary/jinarybackend/controller/TestController.java
+++ Jinary-Backend/src/main/java/jinary/jinarybackend/controller/TestController.java
@@ -1,4 +1,20 @@
 package jinary.jinarybackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping(value = "/test/binary", produces = "application/x-protobuf")
+    public byte[] getBinaryData() {
+        com.jinary.test.TestUser.UserResponse user = com.jinary.test.TestUser.UserResponse.newBuilder()
+                .setId(202417051)
+                .setName("Sanghwa")
+                .setEmail("test@skhu.ac.kr")
+                .build();
+
+        // 바이너리로 변환하여 전송
+        return user.toByteArray();
+    }
 }
